f2348867de38e62e58492e6f95a6a16723fdff65
Carlos Paparoni <carlos.paparoni@gmail.com>
2019-06-06 01:18:36 -0400

    Moved 'menciones' views/methods to own folder.

--- admin/menciones/verMenciones.php
+++ admin/menciones/verMenciones.php
@@ -1,34 +1,34 @@
-<?php require('../sesion.php'); ?>
-<?php require('../config.php'); ?>
+<?php require('../../sesion.php'); ?>
+<?php require('../../config.php'); ?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Ver menciones</title>
     <meta charset="utf-8">
-    <link rel="icon" type="image/jpg" href="../img/logo.jpg">
-    <script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+    <link rel="icon" type="image/jpg" href="../../img/logo.jpg">
+    <script type="text/javascript" src="../../js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="../../js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
 </head>
 
 <body>
 
     <header>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #AAA9AB">
-            <a class="navbar-brand" href="../paginaP.php">Programa de Profesionalización Docente de la ULA</a>
+            <a class="navbar-brand" href="../../paginaP.php">Programa de Profesionalización Docente de la ULA</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="falsse" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link text-light" href="../registroEs.php">Registre un
+                    <li class="nav-item"><a class="nav-link text-light" href="../../registroEs.php">Registre un
 							estudiante</a></li>
-                    <li class="nav-item"><a class="nav-link text-light" href="../consultaEs.php">Consulte la información
+                    <li class="nav-item"><a class="nav-link text-light" href="../../consultaEs.php">Consulte la información
 							de un estudiante</a></li>
-                    <li class="nav-item"><a class="nav-link text-light" href="../registroPa.php">Registre un pago</a></li>
-                    <li class="nav-item"><a class="nav-link text-light" href="../consultaPa.php">Consulte un
+                    <li class="nav-item"><a class="nav-link text-light" href="../../registroPa.php">Registre un pago</a></li>
+                    <li class="nav-item"><a class="nav-link text-light" href="../../consultaPa.php">Consulte un
 							pago</a></li>
                 </ul>
             <!--
